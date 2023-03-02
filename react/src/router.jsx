b247ed281f8842a4428297b56d3b654343c92da9
Krister Ross <krister.ross@avensia.com>
2023-03-02 23:55:08 +0100

    added defaultlayout and guestlayout and installed headlesssui/react

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,37 +1,50 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
 import Campaigns from "./views/Campaigns";
 import Dashboard from "./views/Dashboard";
 import Growth from "./views/Growth";
 import User from "./views/User";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./views/Dashboard";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Dashboard />
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: '/campaigns',
+                element: <Campaigns />
+            },
+            {
+                path: '/growth',
+                element: <Growth />
+            },
+            {
+                path: '/user',
+                element: <User />
+            },
+        ]
     },
     {
-        path: '/campaigns',
-        element: <Campaigns />
-    },
-    {
-        path: '/growth',
-        element: <Growth />
-    },
-    {
-        path: '/user',
-        element: <User />
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/signup',
-        element: <Signup />
-    },
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
+    }
 ])
 
 export default router;
